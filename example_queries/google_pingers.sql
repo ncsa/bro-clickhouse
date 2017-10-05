c6294c94d062136679c12e5a62207743ff45b814
Justin Azoff <jazoff@illinois.edu>
2017-10-05 11:55:35 -0400

    limit to only icmp echo

--- example_queries/google_pingers.sql
+++ example_queries/google_pingers.sql
@@ -12,7 +12,8 @@ FROM
         orig_h,
         sum(orig_pkts) AS pkts
     FROM conn
-    WHERE (day > '2017-09-01') AND (proto = 'icmp') AND (resp_h = '8.8.8.8')
+    -- orig_p is icmp type, type 8 == echo.
+    WHERE (day > '2017-09-01') AND (proto = 'icmp') AND (resp_h = '8.8.8.8') AND (orig_p == 8)
     GROUP BY
         day,
         orig_h
